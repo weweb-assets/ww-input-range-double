8ead444db00f82b4af26de1404420a88d53d2363
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-06-16 22:29:23 +0200

    First commit 🐠

---
+++ ww-config.js
@@ -0,0 +1,16 @@
+export default {
+  editor: {
+    label: {
+      en: "Double range input",
+    },
+  },
+  properties: {
+    textColor: {
+      label: {
+        en: "Text color",
+      },
+      type: "Color",
+      defaultValue: "#F23636",
+    },
+  },
+};
